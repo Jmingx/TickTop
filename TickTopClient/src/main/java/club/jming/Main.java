6ca89203ac34050a9cbc31cd8de6c9dd3c1c9497
JMing <782893110@qq.com>
2021-08-23 01:09:32 +0800

    新增日志信息

--- TickTopClient/src/main/java/club/jming/Main.java
+++ TickTopClient/src/main/java/club/jming/Main.java
@@ -1,10 +1,12 @@
 package club.jming;
 
 import club.jming.exec.Client;
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.concurrent.BrokenBarrierException;
 
+@Slf4j
 public class Main {
-
 
     public static void main(String[] args) throws InterruptedException, BrokenBarrierException {
         Client.exec(16);
--- TickTopClient/src/main/java/club/jming/exec/Client.java
+++ TickTopClient/src/main/java/club/jming/exec/Client.java
@@ -3,7 +3,6 @@ package club.jming.exec;
 import club.jming.entity.Msg;
 import club.jming.utils.ClientUtil;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Properties;
@@ -11,11 +10,13 @@ import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
+@Slf4j
 public class Client {
 
     /**
@@ -46,7 +47,8 @@ public class Client {
         CyclicBarrier end = new CyclicBarrier(threads + 1);
         CyclicBarrier start = new CyclicBarrier(threads + 1);
         //每个生产者发送的消息数
-        int times = 100000;
+        int times = 1000;
+
         //TODO 用配置文件写好用例的参数，读取到内存中...
 
         while (true) {
@@ -55,16 +57,16 @@ public class Client {
             /*2.指定连接的kafka集群, broker-list */
             props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.160.133:9092");
             /*3.ack应答级别*/
-//            props.put(ProducerConfig.ACKS_CONFIG, "-1");
-//            /*4.重试次数*/
-//            props.put(ProducerConfig.RETRIES_CONFIG, 1);
-//            /*5.批次大小，一次发送多少数据，当数据大于16k，生产者会发送数据到 kafka集群 */
-//            props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16 * 1024);
-//            /*6.等待时间， 等待时间超过1毫秒，即便数据没有大于16k， 也会写数据到kafka集群 */
-//            props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
-//            /*7. RecordAccumulator 缓冲区大小*/
-//            props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 32 * 1024 * 1024);
-//            /*8. key, value 的序列化类 */
+            props.put(ProducerConfig.ACKS_CONFIG, "-1");
+            /*4.重试次数*/
+            props.put(ProducerConfig.RETRIES_CONFIG, 1);
+            /*5.批次大小，一次发送多少数据，当数据大于16k，生产者会发送数据到 kafka集群 */
+            props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16 * 1024);
+            /*6.等待时间， 等待时间超过1毫秒，即便数据没有大于16k， 也会写数据到kafka集群 */
+            props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+            /*7. RecordAccumulator 缓冲区大小*/
+            props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 32 * 1024 * 1024);
+            /*8. key, value 的序列化类 */
             props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
             props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
             //启动线程
@@ -72,12 +74,13 @@ public class Client {
                 KafkaProducer<String, String> producer = new KafkaProducer<>(props);
                 new Thread(new KafkaProducerThread(producer, times, start, end,msg02)).start();
             }
+            log.info(props.toString());
             start.await();
             long first = System.currentTimeMillis();
             end.await();
             long second = System.currentTimeMillis();
             long useTime = second - first;
-            System.out.println("use time : " + useTime);
+            log.info("use time : " + useTime);
 
             //计算CPU、内存、kafka发送速率
             float CPUMsg = 0;
@@ -86,13 +89,11 @@ public class Client {
                 CPUMsg += msg.getCPUMsg();
                 memoryMsg += msg.getMemoryMsg();
             }
-//            System.out.println(list.size());
-//            System.out.println(Arrays.toString(list.toArray()));
-            System.out.println("CPU:" + CPUMsg / list.size());
-            System.out.println("Memory:" + memoryMsg / list.size());
+            log.info("CPU:{}" , CPUMsg / list.size());
+            log.info("Memory:{}" , memoryMsg / list.size());
             float speed = 0;
             speed = 100.0f * (times * threads) / (useTime);
-            System.out.println("kafka speed : " + speed + "/ms");
+            log.info("kafka speed : {}/ms",speed);
             list.clear();
         }
     }
